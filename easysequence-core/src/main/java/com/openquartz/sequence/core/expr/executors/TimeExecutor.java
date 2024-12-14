bba3034657e45e019e67f4e7190eda95e98b5884
svnlab <openquartz@gmail.com>
2024-12-14 20:28:41 +0800

    feat(): 时间戳支持

--- easysequence-core/src/main/java/com/openquartz/sequence/core/expr/executors/TimeExecutor.java
+++ easysequence-core/src/main/java/com/openquartz/sequence/core/expr/executors/TimeExecutor.java
@@ -1,34 +1,61 @@
 package com.openquartz.sequence.core.expr.executors;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.openquartz.sequence.core.expr.cmd.Environment;
 import com.openquartz.sequence.core.expr.cmd.Command;
 import com.openquartz.sequence.core.expr.cmd.Result;
 import com.openquartz.sequence.generator.common.exception.Asserts;
 import com.openquartz.sequence.generator.common.exception.CommonErrorCode;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * @author svnee
  */
 public class TimeExecutor implements CommandExecutor {
 
-    private static final List<String> AVAILABLE_TIME_FORMATS = Lists.newArrayList(
+    /**
+     * 时间戳格式
+     * Timestamp: 毫秒时间错
+     * Timestamp-s: 秒时间戳
+     */
+    private static final Set<String> AVAILABLE_TIME_FORMATS = Sets.newHashSet(
         "yyyyMMdd",
         "yyyyMMddHHmmss",
         "yyyy-MM-dd",
         "yyyyMMddHHmmssSSS",
         "yyMMddHHmmss",
-        "yyMMdd"
+        "yyMMdd",
+        "Timestamp",
+        "Timestamp-s"
     );
 
     @Override
     public Result exec(Command command) {
+
+        LocalDateTime localTime = LocalDateTime.now();
+        String formatResult = parseTimeFormat(localTime, command.getParams().get(0));
+        if (Objects.nonNull(formatResult)) {
+            return Result.success(formatResult);
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(command.getParams().get(0));
-        String formatResult = formatter.format(LocalDateTime.now());
+        formatResult = formatter.format(localTime);
         return Result.success(formatResult);
+    }
+
+    private String parseTimeFormat(LocalDateTime localTime, String timestampPattern) {
+
+        switch (timestampPattern) {
+            case "Timestamp":
+                return String.valueOf(localTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+            case "Timestamp-s":
+                return String.valueOf(localTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond());
+            default:
+                return null;
+        }
     }
 
     @Override
